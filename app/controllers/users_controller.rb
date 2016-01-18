cffffb241a764e8f004d5f2cc8660a7acd7dbb8f
builditdan <daniels111@gmail.com>
2016-01-18 12:18:53 -0500

    Added new code to the user profile screen and users are  routed to that screen at login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,13 @@ class UsersController < ApplicationController
 
 
   def show
+    redirect_to (welcome_index_path) if current_user.blank?
     @user = current_user
       #@posts = @user.posts.visible_to(current_user)
   end
 
   def new
   	end
-  	
+
   #### end class
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   get 'welcome/about'
+  get 'welcome/index'
   get 'users/show'
 
-  root to: 'welcome#index'
+  #root to: 'welcome#index'
+  root to: 'users#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
